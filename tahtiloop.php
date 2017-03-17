236a115ef36f153e159c122cb27f1b953d9d3af9
essikun <essikun@h-edu-mba13-06.local>
2017-03-17 09:57:36 +0200

    testi

---
+++ tahtiloop.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="tahtiloop.php">
+  Valitse tähtien määrä:
+  <input type="number" name="maara" min="1" max="100">
+  <input type="submit">
+</form>
+    <br>
+    
+<?php
+$maara = $_POST['maara'];
+
+for($i=1;$i<=$maara;$i++){
+for($j=1;$j<=$i;$j++){
+echo "*&nbsp;";
+}
+echo "<br>";
+}
+?>
+
+    
+</body>
+</html>
