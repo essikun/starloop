94363f499e2a3394e4fc43bf5d246dcae5adb7b9
Samppa <samppa.sassi@gmail.com>
2017-03-17 10:30:27 +0200

    Merkittäviä parannuksia koodiin.

--- tahtiloop.php
+++ tahtiloop.php
@@ -1,7 +1,22 @@
 <!DOCTYPE html>
 <html>
+<head>
+<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js'>
+</script>
+<script>
+    $(document).ready(function(){
+        $("button").click(function(){
+            $("p").toggle();
+        });
+    });
+</script>
+</head>
 <body>
-
+    
+<p>Lorem Ipsum jne</p>
+    
+<button>Nappi</button>
+<br />
 <form method="post" action="tahtiloop.php">
   Valitse tähtien määrä:
   <input type="number" name="maara" min="1" max="100">
